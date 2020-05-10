e1b986ce1ef85da106f700c72a92acb02a6d89b5
Daniel Bass <danielbass123456@gmail.com>
2020-05-10 14:10:14 -0500

    Version 0.0.1 release
    Meme get and voting available

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const multer = require('multer')
 app.use( express.json() )
 app.use( multer().any() )
 
-app.listen(3000, () => {
-    console.log('listening on 3000')
+app.listen(9000, () => {
+    console.log('listening on 9000')
 });
 
 const url = 'http://leinad.pw/rmeme/images/memes/'
@@ -19,15 +19,14 @@ app.get('/rmeme', (req, res) => {
         var images = JSON.parse(fs.readFileSync('./images.json', 'utf8'));
         var num = Math.floor(images.size * Math.random())
         var file = images.images[num]
+        // console.log(file)
         if (file.format === 'JPEG') { var format = 'jpg' }
         res.status(200).json({
             id: num,
             name: file.name,
             url: `${url}${file.name}.${format.toLocaleLowerCase()}`,
             format: file.format,
-            score: file.score,
-            author: 'anonymous', // todo
-            created: new Date() // todo
+            score: file.score
         });
     } catch(e) {
         console.log(e)
@@ -41,16 +40,14 @@ app.get('/rmeme/:id', (req, res) => {
         id = req.params.id
         var images = JSON.parse(fs.readFileSync('./images.json', 'utf8'));
         var file = images.images[id]
-        console.log(id)
-        if (file.format === 'JPEG') { var format = 'jpg' }
+        // console.log(file)
+        var format = (file.format === 'JPEG' ? 'jpg' : file.format.toLowerCase())
         res.status(200).json({
             id: id,
             name: file.name,
-            url: `${url}${file.name}.${format.toLocaleLowerCase()}`,
+            url: `${url}${file.name}.${format}`,
             format: file.format,
-            score: file.score,
-            author: 'anonymous',
-            created: new Date()
+            score: file.score
         });
     } catch(e) {
         console.log(e)
@@ -71,10 +68,48 @@ app.get('/rmeme/memes/total', (req, res) => {
 
 
 // put requests ////////////////////////////
-app.put('/rmeme/:id', (req, res) => {
-    console.log("will update a meme by id")
-    res.send('200')
+app.put('/rmeme/:id/up', (req, res) => {
+    try {
+        id = req.params.id
+        var images = JSON.parse(fs.readFileSync('./images.json', 'utf8'));
+        var file = images.images[id]
+        // console.log(file)
+        file.score += 1
+        fs.writeFileSync('./images.json', JSON.stringify(images, undefined, 2))
+        if (file.format === 'JPEG') { var format = 'jpg' }
+        res.status(200).json({
+            id: id,
+            name: file.name,
+            url: `${url}${file.name}.${format.toLocaleLowerCase()}`,
+            format: file.format,
+            score: file.score
+        })  
+    } catch(e) {
+        console.log(e)
+        res.status(500).send(`Error when upvoting meme ${req.params.id}`)
+    }
 });
+
+app.put('/rmeme/:id/down', (req, res) => {
+    try {
+        id = req.params.id
+        var images = JSON.parse(fs.readFileSync('./images.json', 'utf8'));
+        var file = images.images[id]
+        file.score -= 1
+        fs.writeFileSync('./images.json', JSON.stringify(images, undefined, 2))
+        if (file.format === 'JPEG') { var format = 'jpg' }
+        res.status(200).json({
+            id: id,
+            name: file.name,
+            url: `${url}${file.name}.${format.toLocaleLowerCase()}`,
+            format: file.format,
+            score: file.score
+        })  
+    } catch(e) {
+        console.log(e)
+        res.status(500).send(`Error when downvoting meme ${req.params.id}`)
+    }
+})
 ///////////////////////////////////////////
 
 
@@ -90,7 +125,23 @@ app.post('/rmeme/:id', (req, res) => {
 
 // delete requests //////////////////////
 app.delete('/rmeme/del/:id', (req, res) => {
-    console.log('will delete a meme by id')
-    res.send('200')
+    res.status(200).send('This is currently under construction.')
+    // try {
+    //     id = req.params.id
+    //     var images = JSON.parse(fs.readFileSync('./images.json', 'utf8'));
+    //     var size = images.size
+    //     if (id == size) {
+    //         delete images[id]
+    //     } else {
+    //         images.images[id] = images.images[size-1]
+    //         delete images.images[size-1]
+    //     }
+    //     images.size -= 1
+    //     fs.writeFileSync('./images.json', JSON.stringify(images, undefined, 2))
+    //     res.status(200).send(`Successfully deleted meme ${id}`)  
+    // } catch(e) {
+    //     console.log(e)
+    //     res.status(500).send(`Error when deleting meme ${req.params.id}`)
+    // }
 });
 ////////////////////////////////////////
