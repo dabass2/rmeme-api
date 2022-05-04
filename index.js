bf6d6bc8553f8cef5a1320a03572a5a95c6f43c8
Daniel Bass <danielbass123456@gmail.com>
2022-05-03 22:11:03 -0500

    emergency patch

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.listen(9000, () => {
 
 // Sql stuff here, probably from secret config file
 
-const url = 'https://rmeme.me/images/memes/'
+// const url = 'https://rmeme.me/images/memes/'
+const url = 'https://leinad.dev/images/memes/'
 const requests = {"get": 0, "put": 1, "post": 2, "delete": 3}
 
 function createToken() {
