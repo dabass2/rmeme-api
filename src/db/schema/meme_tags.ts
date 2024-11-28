250de0baf7f0c95359389a7fe13ae25cd7d36ec8
Daniel Bass <danielbass123456@gmail.com>
2024-11-27 18:12:52 -0600

    feat: swap to deno

--- src/db/schema/meme_tags.ts
+++
@@ -1,8 +0,0 @@
-import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
-import { memes } from "./memes";
-
-export const meme_tags = mysqlTable("meme_tags", {
-  tag_id: int("tag_id").primaryKey().notNull().autoincrement(),
-  tag: varchar("tag", { length: 255 }).notNull(),
-  meme_id: int("meme_id").references(() => memes.meme_id),
-});
